456501c3dbf5942e1156cd2de461fa438d961255
shfanfan <shfanfan@yahoo.com>
2017-01-28 02:11:31 +0200

    Add files via upload
    initial file - controlling two steppers

---
+++ test1.ino
@@ -0,0 +1,59 @@
+
+int stepDirection = 8;
+int stepTrigger = 9;
+int dir;
+char steps;
+int x=0;
+
+
+void step(){
+    digitalWrite(stepTrigger, HIGH);   
+    delayMicroseconds(700);     
+    digitalWrite(stepTrigger, LOW);   
+    delayMicroseconds(700);  
+}
+
+
+void setup() {
+  // pins for steper
+  pinMode(stepDirection, OUTPUT);
+  pinMode(stepTrigger, OUTPUT);
+    
+  // initialize serial communication:
+  Serial.begin(9600);
+}
+
+           
+           
+    // the loop function runs over and over again forever
+    void loop() {
+
+      
+        if (Serial.ready){
+            steps = Serial.read();
+            if (steps<0)
+            {
+                digitalWrite(stepDirection, HIGH);
+                steps = steps * -1;
+            }else {
+                digitalWrite(stepDirection, LOW);
+            }
+            
+            for  (x=0; x<steps ; x++)
+            {
+                step();
+            }
+            
+            
+        }
+        
+}
+
+
+
+
+
+
+
+
+
