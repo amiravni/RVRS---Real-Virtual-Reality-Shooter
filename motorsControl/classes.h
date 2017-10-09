0647ebc73a6f3e7bd690746d7aa62e1b49cc1057
amir avni <amiravni83@gmail.com>
2017-10-09 23:57:54 +0300

    Add step motor and PID

--- motorsControl/classes.h
+++ motorsControl/classes.h
@@ -1,4 +1,3 @@
-
 class motor {
   public:
     motor(int mA, int mB, int eA, int eB, int mPWM) {
@@ -17,64 +16,90 @@ class motor {
       digitalWrite(motorEncA, LOW); //initialize Pin States
       digitalWrite(motorEncB, LOW);
       motorPWMPinVal = 255;
+      motorDirection = true;
       analogWrite(motorPWMPin, motorPWMPinVal);
-      if (motorEncA == INT0) {
+      if (motorEncA == INT0PIN) {
         encoder0PinA = motorEncA;
         encoder0PinB = motorEncB;
 
       }
-      else if (motorEncA == INT1) {
+      else if (motorEncA == INT1PIN) {
         encoder1PinA = motorEncA;
         encoder1PinB = motorEncB;
       }
       currentEncoderValue = 0;
     }
 
+    motor(int _stepPin, int _dirPin) {
+      motorPinA = _stepPin;
+      motorPinB = _dirPin;
+      pinMode(motorPinA, OUTPUT); //initialize Encoder Pins
+      pinMode(motorPinB, OUTPUT);   
+      digitalWrite(motorPinA, LOW); //initialize Pin States
+      digitalWrite(motorPinB, LOW);
+    }
+
+    void moveStepMotor(int steps, boolean dir, int dly) {
+      digitalWrite(motorPinB, dir);
+      for (int iii = 0 ; iii < steps ; iii++) {
+        digitalWrite(motorPinA, HIGH);
+        delay(dly);
+        digitalWrite(motorPinA, LOW);
+        delay(dly);
+      }
+    }
+
     void setCommand(long numOfSteps) {
       lastStepsCommand = numOfSteps;
     }
-    
-    void moveMotorCCW(int PWMVal, boolean moving) {
+
+    void moveMotorCCW(int PWMVal) {
       analogWrite(motorPWMPin, PWMVal);
       digitalWrite(motorPinA, HIGH);
       digitalWrite(motorPinB, LOW);
       PRINTLNDEBUG(" CCW ");
-      if (moving) lastCommand = -1;
     }
 
-    void moveMotorCW(int PWMVal, boolean moving) {
+    void moveMotorCW(int PWMVal) {
       analogWrite(motorPWMPin, PWMVal);
       digitalWrite(motorPinA, LOW);
       digitalWrite(motorPinB, HIGH);
       PRINTLNDEBUG(" CW ");
-      if (moving) lastCommand = 1;
     }
 
     void moveMotorStop() {
       digitalWrite(motorPinA, LOW);
       digitalWrite(motorPinB, LOW);
       PRINTLNDEBUG(" ST ");
-    }    
+    }
 
-    void moveMotor() {
-      if (motorEncA == INT0) currentEncoderValue = counts0;
-      if (motorEncA == INT1) currentEncoderValue = counts1;
-      //int motorPWMPinVal = max(128,min(255,int(abs(lastStepsCommand - currentEncoderValue) / 2 )));
-      //analogWrite(motorPWMPin, motorPWMPinVal);
+    void moveMotorPID() {
+      if (motorDirection) moveMotorCW(motorPWMPinVal);
+      else moveMotorCCW(motorPWMPinVal);
+    }
+
+    void moveMotor(double PIDOutput) {
+      motorPWMPinVal = abs((int)PIDOutput);
+      motorDirection = (PIDOutput > 0);
+      if (motorEncA == INT0PIN) currentEncoderValue = counts0;
+      if (motorEncA == INT1PIN) currentEncoderValue = counts1;
+      analogWrite(motorPWMPin, motorPWMPinVal);
       PRINTDEBUG(currentEncoderValue);
       PRINTDEBUG(" , ");
+      PRINTDEBUG(currentEncoderValue - counts1Last);
+      PRINTDEBUG(" , ");      
       PRINTDEBUG(lastStepsCommand);
       PRINTDEBUG(" , ");
-      motorPWMPinVal = 255;
-      if (lastStepsCommand - currentEncoderValue < -100) moveMotorCCW(motorPWMPinVal,true);
-      else if (lastStepsCommand - currentEncoderValue > 100) moveMotorCW(motorPWMPinVal,true);
-      else { /*
-        motorPWMPinVal = 170;
-        if (lastCommand == -1) moveMotorCW(motorPWMPinVal,false);
-        else if (lastCommand == 1) moveMotorCCW(motorPWMPinVal,false);
-        if (abs(lastStepsCommand - currentEncoderValue) < 20)  moveMotorStop();*/
-        moveMotorStop();
-      }
+      PRINTDEBUG(PIDOutput);
+      PRINTDEBUG(" , ");      
+      PRINTDEBUG(motorPWMPinVal);
+      PRINTDEBUG(" , ");  
+      counts1Last = currentEncoderValue;       
+      moveMotorPID();
+      /*if (lastStepsCommand - currentEncoderValue < -10) moveMotorCCW(motorPWMPinVal, true);
+      else if (lastStepsCommand - currentEncoderValue > 10) moveMotorCW(motorPWMPinVal, true);
+      else  moveMotorStop();*/
+      
     }
 
 
@@ -87,6 +112,7 @@ class motor {
     long lastStepsCommand;
     int motorPWMPin;
     int motorPWMPinVal;
+    boolean motorDirection;
     int lastCommand = 0;
 };
 
--- motorsControl/motorsControl.ino
+++ motorsControl/motorsControl.ino
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <math.h>
-
+#include <PID_v1.h>
 
 #define DEBUG 1
 #define PRINTDEBUG(STR) \
@@ -25,8 +25,13 @@
 #define Motor1EncoderPinB 12   // Encoder Pin B
 #define Motor1PWM 11
 
-#define INT0 2  // <-- change this when not using Arduino Uno
-#define INT1 3  // <-- change this when not using Arduino Uno
+#define stepMotorStepPin 6
+#define stepMotorDirPin 13
+
+#define INT0PIN 2  // <-- change this when not using Arduino Uno
+#define INT1PIN 3  // <-- change this when not using Arduino Uno
+
+#define PIDLIM 100000
 
 //Initialize Variables
 int encoder0PinA = -1;
@@ -35,32 +40,86 @@ int encoder1PinA = -1;
 int encoder1PinB = -1;
 long counts0 = 0; //counts the encoder counts.
 long counts1 = 0; //counts the encoder counts.
+long counts1Last = 0;
 long commandTest = 0;
-long commandTestUpdate = millis();
+long commandTestUpdate = -60000;//millis();
+
+double Setpoint = 0;
+double Input = 0;
+double Output = 0;
+int kp = 50;
+int ki = 5;
+int kd = 0;
+double k_amp = 3.75;
 
 #include "classes.h"
 
 motor motorA(Motor0PinA, Motor0PinB, Motor0EncoderPinA, Motor0EncoderPinB , Motor0PWM);
 motor motorB(Motor1PinA, Motor1PinB, Motor1EncoderPinA, Motor1EncoderPinB , Motor1PWM);
+motor stepMotor(stepMotorStepPin, stepMotorDirPin);
+PID myPID(&Input, &Output, &Setpoint, kp, ki, kd, P_ON_E, DIRECT); //P_ON_M specifies that Proportional on Measurement be used
 
 void setup() {
   if (DEBUG) Serial.begin(115200);
-  attachInterrupt(0, readEncoder0, CHANGE); //attach interrupt to PIN 2
+  //attachInterrupt(0, readEncoder0, CHANGE); //attach interrupt to PIN 2
   attachInterrupt(1, readEncoder1, CHANGE); //attach interrupt to PIN 3
+  myPID.SetMode(AUTOMATIC);
+  myPID.SetOutputLimits(-PIDLIM, PIDLIM);
+
 }
 
 void loop() {
-  motorA.moveMotor();
-  motorB.moveMotor();
-  if (millis() - commandTestUpdate > 1000) {
+  //motorA.moveMotor();
+  Setpoint = (double)commandTest;
+  Input = counts1;
+
+  myPID.Compute();
+  double nOutput = constrain(map(Output, -PIDLIM*k_amp, PIDLIM*k_amp, -255, 255),-255,255);
+  if (abs(nOutput) > 3 && abs(nOutput) < 29) {
+    if (nOutput > 0) nOutput = 29;
+    else nOutput = -29;
+  }
+  motorB.moveMotor(nOutput);
+
+  if (millis() - commandTestUpdate > 60000) {
     commandTestUpdate = millis();
-    PRINTLNDEBUG(counts0);
-    commandTest = commandTest + 1120;
+    PRINTLNDEBUG(counts1);
+    commandTest = commandTest + 1120*6;
     motorA.setCommand(commandTest);
     motorB.setCommand(commandTest);
-
+    stepMotor.moveStepMotor(50,true,1);
+/*    for (int iii = 0; iii < 20; iii++) {
+    myPID.Compute();
+    PRINTDEBUG(Setpoint);
+    PRINTDEBUG(" , ");
+    PRINTLNDEBUG(Output);
+    }
+    while(1) {
+      
+    }*/
   }
 
+  /* Stepper Test */ /*
+    digitalWrite(dir, LOW);
+    for (int jjj = 0 ; jjj < 4 ; jjj++) {
+    for (int iii = 0 ; iii < 50 ; iii++) {
+      digitalWrite(stp, HIGH);
+      delay(1);
+      digitalWrite(stp, LOW);
+      delay(1);
+    }
+
+    delay(1000);
+    }
+    digitalWrite(dir, HIGH);
+    for (int iii = 0 ; iii < 50 ; iii++) {
+      digitalWrite(stp, HIGH);
+      delay(1);
+      digitalWrite(stp, LOW);
+      delay(1);
+    }
+    delay(1000);
+  */
 }
 
 void readEncoder0() //this function is triggered by the encoder CHANGE, and increments the encoder counter
